ea2f0dfd6d45409e4e92e3249a9e0095b7f74fc0
cecilialsantin <chikhakituti@gmail.com>
2022-10-20 22:36:54 -0300

    pie de pagina dinamico para completar

---
+++ js/main.js
@@ -0,0 +1,14 @@
+//Agregado pie de pagina
+
+let piepags = document.getElementById('piePagina');
+
+piepags.innerHTML = `
+
+<ul>
+    <li>Copyright</li>
+    <li>1</li>
+    <li>2</li>
+    <li>3</li>
+    <li>4</li>
+</ul>
+`

4b69f86300f322847f3fdb37eabaade5fa9c1e46
cecilialsantin <chikhakituti@gmail.com>
2022-10-22 08:44:13 -0300

    cambios en estilos y pagina contacto a la que le falta mostrar la imagen y el link de sucursales

--- js/main.js
+++ js/main.js
@@ -3,12 +3,39 @@
 let piepags = document.getElementById('piePagina');
 
 piepags.innerHTML = `
-gir 
-<ul>
-    <li>Copyright</li>
-    <li>1</li>
-    <li>2</li>
-    <li>3</li>
-    <li>4</li>
-</ul>
+    <p>Copyright - Grupo Botanica -  </p>
+    <p>Curso Full Stack Python Codo a Codo - Comisión 22519</p>
 `
+
+//animación imagen Contacto
+
+imgContacto.onclick= function(){
+    this.style.width = '200px';
+    this.style.height = '200px';
+}; 
+
+//carga de sucursales en base a la clase sucursal
+
+
+const sucursales = [
+    new Sucursal('Reserva Pilar', 'Pilar', 'repi@mail.com.ar', new Image(50, '/src/reservaPilar.png'), new URL('https://goo.gl/maps/cWKPUZLYTBv8YyCAA')),
+    new Sucursal('Reserva Quinta Gigordia', 'Luján', 'quicig@mail.com.ar', new Image(50, '/src/quintaCigordia.jpeg'), new URL('https://goo.gl/maps/VJbKy7fT2RRoPMLy9')),
+];
+
+function mostrarSucursales(){
+    console.log("Mostrar Sucursales");
+    let texto='';
+   for(let sucursal of sucursales){
+        console.log(sucursales);
+        texto += `<tr>
+			<td>${sucursal.nombre}</td>
+			<td>${sucursal.localidad}</td>
+			<td>${sucursal.email}</td>
+			<td>${sucursal.foto}</td>
+			<td>${sucursal.mapa}</td>
+            <tr>`
+    }
+
+    document.getElementById('sucursales').innerHTML = texto;
+
+}

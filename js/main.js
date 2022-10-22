0e9863ed30c179de55a154d19fe794c88d7bdcd0
cecilialsantin <chikhakituti@gmail.com>
2022-10-22 11:11:14 -0300

    listo contacto.html con las sucursales dinamicas

--- js/main.js
+++ js/main.js
@@ -18,8 +18,8 @@ imgContacto.onclick= function(){
 
 
 const sucursales = [
-    new Sucursal('Reserva Pilar', 'Pilar', 'repi@mail.com.ar', new Image(50, '/src/reservaPilar.png'), new URL('https://goo.gl/maps/cWKPUZLYTBv8YyCAA')),
-    new Sucursal('Reserva Quinta Gigordia', 'Luján', 'quicig@mail.com.ar', new Image(50, '/src/quintaCigordia.jpeg'), new URL('https://goo.gl/maps/VJbKy7fT2RRoPMLy9')),
+    new Sucursal('Reserva Pilar', 'Pilar', 'repi@mail.com.ar', '/src/reservaPilar.png', 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3291.5550293167357!2d-58.9159119!3d-34.4126543!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95bc9d54387b4de7%3A0xb3cfe58ae07fc0db!2sReserva%20Natural%20del%20Pilar!5e0!3m2!1ses-419!2sar!4v1666446330569!5m2!1ses-419!2sar'),
+    new Sucursal('Reserva Quinta Gigordia', 'Luján', 'quicig@mail.com.ar','/src/quintaCigordia.jpeg', 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3285.0990358731915!2d-59.130341484857325!3d-34.57636056355795!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95bc7dbd9ba47c89%3A0xf1e73fb8b4a42bd1!2sReserva%20Forestal%20Quinta%20de%20Cigordia!5e0!3m2!1ses-419!2sar!4v1666446546067!5m2!1ses-419!2sar'),
 ];
 
 function mostrarSucursales(){
@@ -30,9 +30,9 @@ function mostrarSucursales(){
         texto += `<tr>
 			<td>${sucursal.nombre}</td>
 			<td>${sucursal.localidad}</td>
-			<td>${sucursal.email}</td>
-			<td>${sucursal.foto}</td>
-			<td>${sucursal.mapa}</td>
+			<td><a href="mailto:${sucursal.email}">${sucursal.email}</a></td>
+			<td><img src=${sucursal.foto}></td>
+			<td><iframe src=${sucursal.mapa}></iframe></td>
             <tr>`
     }
 
